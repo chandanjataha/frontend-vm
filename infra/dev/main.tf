79b14884d4fb764393ee7d0728457c09918b7d5b
chandanjataha <chandanjataha@gmail.com>
2025-06-20 22:23:40 +0530

    formating

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -4,45 +4,45 @@ module "md-rg" {
 }
 
 module "md-vnet" {
-    source = "../../module/azure-vnet"
-    vnet = var.pvnet
-    depends_on = [ module.md-rg ]
+  source     = "../../module/azure-vnet"
+  vnet       = var.pvnet
+  depends_on = [module.md-rg]
 }
 
 module "md-stg" {
-  source = "../../module/azure-stg"
-  stg = var.pstg
-  depends_on = [ module.md-rg ]
+  source     = "../../module/azure-stg"
+  stg        = var.pstg
+  depends_on = [module.md-rg]
 }
 
-module  "md-snet" {
-  source = "../../module/azure-snet"
-  snet = var.psnet
-  depends_on = [ module.md-vnet ]
+module "md-snet" {
+  source     = "../../module/azure-snet"
+  snet       = var.psnet
+  depends_on = [module.md-vnet]
 }
 module "md-pip" {
-  source = "../../module/azure-pip"
-  pip = var.ppip
-  depends_on = [ module.md-rg ]
+  source     = "../../module/azure-pip"
+  pip        = var.ppip
+  depends_on = [module.md-rg]
 }
 
 module "md-nic" {
-  source = "../../module/azure-nic"
-  nic = var.pnic
-  snet = var.psnet
-  pip = var.ppip
-  depends_on = [ module.md-snet, module.md-pip ]
+  source     = "../../module/azure-nic"
+  nic        = var.pnic
+  snet       = var.psnet
+  pip        = var.ppip
+  depends_on = [module.md-snet, module.md-pip]
 }
 
 module "md-vm" {
-  source = "../../module/azure-vm"
-  vm = var.pvm
-  nic = var.pnic
-  depends_on = [ module.md-nic ]
+  source     = "../../module/azure-vm"
+  vm         = var.pvm
+  nic        = var.pnic
+  depends_on = [module.md-nic]
 }
 
 module "md-nsg" {
-  source = "../../module/azure-nsg"
-  nsg = var.pnsg
-  depends_on = [ module.md-rg ]
+  source     = "../../module/azure-nsg"
+  nsg        = var.pnsg
+  depends_on = [module.md-rg]
 }
